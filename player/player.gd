0da66c8da025d676216727cb763a92959cf3485e
Hushripple <vaenuslazorbeamz@gmail.com>
2024-10-31 16:38:12 -0300

    2: Creación del personaje principal + movimiento

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
+# Velocidad del jugador 
 @export var speed: int = 35
 
+# Controles
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
-	
+
+# Movimiento	
 func _physics_process(delta):
 	handleInput()
 	move_and_slide() 
